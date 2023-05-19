44104ca53b16d4061eca4134d2daff95e3d404ee
Fenikz <fenikz31@gmail.com>
2023-05-19 15:13:44 +0200

    first commit

---
+++ scripts/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GH_OWNER=$GH_OWNER
+REG_TOKEN=$REG_TOKEN
+
+RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
+RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+
+cd /home/docker/actions-runner
+
+./config.sh --unattended --url https://github.com/${GH_OWNER} --token ${REG_TOKEN}
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --unattended --token ${REG_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
